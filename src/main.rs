a8e1fc2e627a76bd66c72ad731118da9afc63d01
Tommy Thorn <tommy-git2@thorn.ws>
2022-06-06 15:57:32 -0700

    Drop sentinel, add unit tests, support negatives

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,30 @@
 
 type Source<'a> = &'a[u8];
 
+#[test]
+fn number1() { assert_eq!(parse(b"-42"), Ok(-42)); }
+#[test]
+fn number2() { assert_eq!(parse(b" - 42"), Ok(-42)); }
+#[test]
+fn number3() { assert_eq!(parse(b" 84 - 42"), Ok(42)); }
+#[test]
+fn number4() { assert_eq!(parse(b" 84 + -42"), Ok(42)); }
+
 fn main() {
-    let mut source: Source = b" 42 + 56*78 - 40!"; // End in a sentinel to avoid testing for length everywhere
-    match parse(&mut source) {
-	Ok(v) => println!("Result {v} + {source:?}"),
-	_ => println!("Parse Error"),
+    match parse(b" 42 + 56*78 - 40") {
+	Ok(v) => println!("Result {v}"),
+	Err(s) => println!("{s}"),
     }
 }
 
-fn parse(s: &mut Source) -> Result<isize, String> {
-    skip_whitespace(s);
-    parse_expr(s)
+fn parse(mut s: Source) -> Result<isize, String> {
+    skip_whitespace(&mut s);
+    let r = parse_expr(&mut s);
+    if s.is_empty() {
+	r
+    } else {
+	Err(format!("What the hell is {s:?}?"))
+    }
 }
 
 fn parse_expr(s: &mut Source) -> Result<isize, String> {
@@ -38,9 +51,18 @@ fn parse_term(s: &mut Source) -> Result<isize, String> {
 }
 
 fn parse_factor(s: &mut Source) -> Result<isize, String> {
+    if token_match(s, b"-") {
+	//return -(parse_factor(s)?);
+	return Ok(-(parse_factor(s)?));
+    } else {
+	parse_uint(s)
+    }
+}
+
+fn parse_uint(s: &mut Source) -> Result<isize, String> {
     if b'0' <= s[0] && s[0] <= b'9' {
 	let mut v = 0;
-	while b'0' <= s[0] && s[0] <= b'9' {
+	while !s.is_empty() && b'0' <= s[0] && s[0] <= b'9' {
 	    v = v*10 + (s[0] as isize - b'0' as isize) as isize;
 	    *s = &s[1..];
 	}
@@ -51,18 +73,18 @@ fn parse_factor(s: &mut Source) -> Result<isize, String> {
     }
 }
 
-fn skip_whitespace(s: &mut Source) {
-    while s[0] == b' ' {
-	*s = &s[1..];
-    }
-}
-
 fn token_match<'a>(s: &mut Source<'a>, expected: &[u8]) -> bool {
-    if s[0..expected.len()] == *expected {
+    if s.len() >= expected.len() && s[0..expected.len()] == *expected {
 	*s = &s[expected.len()..];
 	skip_whitespace(s);
 	true
     } else {
 	false
+    }
+}
+
+fn skip_whitespace(s: &mut Source) {
+    while !s.is_empty() && s[0] == b' ' {
+	*s = &s[1..];
     }
 }
