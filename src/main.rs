46c7ece432f962e1ce85ebbcbc5caae1b89c7109
Tommy Thorn <tommy-git2@thorn.ws>
2022-06-07 00:34:12 -0700

    Towards an compiler/interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,79 @@
 // So, you want to write a parser (without pulling in a crate)?
 
 type Source<'a> = &'a [u8];
+type Compiled = isize;
+enum Insn {Add, Sub, Mul, Negate, Const(isize),}
+
 
 #[test]
 fn number1() {
-    assert_eq!(parse(b"-42"), Ok(-42));
+    assert_eq!(run(b"-42"), Ok(-42));
 }
 #[test]
 fn number2() {
-    assert_eq!(parse(b" - 42"), Ok(-42));
+    assert_eq!(run(b" - 42"), Ok(-42));
 }
 #[test]
 fn number3() {
-    assert_eq!(parse(b" 84 - 42"), Ok(42));
+    assert_eq!(run(b" 84 - 42"), Ok(42));
 }
 #[test]
 fn number4() {
-    assert_eq!(parse(b" 84 + -42"), Ok(42));
+    assert_eq!(run(b" 84 + -42"), Ok(42));
 }
 #[test]
 fn number5() {
-    assert_eq!(parse(b"(2)"), Ok(2));
+    assert_eq!(run(b"(2)"), Ok(2));
 }
 #[test]
 fn number6() {
-    assert!(parse(b"(2 + )").is_err());
+    assert!(run(b"(2 + )").is_err());
 }
 #[test]
 fn number7() {
-    assert!(parse(b" ( 2 ").is_err());
+    assert!(run(b" ( 2 ").is_err());
 }
 
 fn main() {
     for s in std::env::args().skip(1) {
-        match parse(s.as_bytes()) {
-	    Ok(v) => println!("Result: {v}"),
-	    Err(s) => println!("Error: {s}"),
-	}
+        match run(s.as_bytes()) {
+            Ok(v) => println!("Result: {v}"),
+            Err(s) => println!("Error: {s}"),
+        }
     }
 }
 
-fn parse(mut s: Source) -> Result<isize, String> {
+fn run(s: Source) -> Result<Compiled, String> {
+    let mut _code : Vec<Insn> = vec![];
+    parse(s)
+    //execute(code)
+}
+
+fn execute(code: Vec<Insn>) -> Result<isize, String> {
+    let mut stack = vec![];
+    let mut tos = 0;
+    for insn in code {
+	match insn {
+	    Insn::Add => tos += stack.pop().ok_or("Run: stack empty")?,
+	    Insn::Sub => tos -= stack.pop().ok_or("Run: stack empty")?,
+	    Insn::Mul => tos *= stack.pop().ok_or("Run: stack empty")?,
+	    Insn::Negate => tos = -tos,
+	    Insn::Const(k) => {
+		stack.push(tos);
+		tos = k;
+	    }
+	}
+    }
+    Ok(tos)
+}
+
+#[test]
+fn execute_test() {
+    use Insn::*;
+    assert_eq!(execute(vec![Const(42),Const(42),Add]), Ok(84));
+}
+
+fn parse(mut s: Source) -> Result<Compiled, String> {
     skip_whitespace(&mut s);
     let r = parse_expr(&mut s);
     if r.is_ok() {
@@ -54,7 +87,7 @@ fn parse(mut s: Source) -> Result<isize, String> {
     }
 }
 
-fn parse_expr(s: &mut Source) -> Result<isize, String> {
+fn parse_expr(s: &mut Source) -> Result<Compiled, String> {
     let mut v = parse_term(s)?;
     loop {
         if token_match(s, b"+") {
@@ -68,7 +101,7 @@ fn parse_expr(s: &mut Source) -> Result<isize, String> {
     Ok(v)
 }
 
-fn parse_term(s: &mut Source) -> Result<isize, String> {
+fn parse_term(s: &mut Source) -> Result<Compiled, String> {
     let mut v = parse_factor(s)?;
     while token_match(s, b"*") {
         v *= parse_factor(s)?;
@@ -76,8 +109,8 @@ fn parse_term(s: &mut Source) -> Result<isize, String> {
     Ok(v)
 }
 
-fn parse_factor(s: &mut Source) -> Result<isize, String> {
-    let begin = s.clone();
+fn parse_factor(s: &mut Source) -> Result<Compiled, String> {
+    let begin = &<&[u8]>::clone(s);
     if token_match(s, b"(") {
         let r = parse_expr(s)?;
         if token_match(s, b")") {
@@ -95,7 +128,7 @@ fn parse_factor(s: &mut Source) -> Result<isize, String> {
     }
 }
 
-fn parse_uint(s: &mut Source) -> Result<isize, String> {
+fn parse_uint(s: &mut Source) -> Result<Compiled, String> {
     if b'0' <= s[0] && s[0] <= b'9' {
         let mut v = 0;
         while !s.is_empty() && b'0' <= s[0] && s[0] <= b'9' {
